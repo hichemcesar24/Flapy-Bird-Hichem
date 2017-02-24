02de98b889d95bf9566d7ceb481d0655e78fdab4
hichemcesar24 <hichem.romdhane@esprit.tn>
2017-02-24 09:48:09 +0100

    Flapy-Bird-Unity-Hichem
    Unity Game 2D using C#

---
+++ Assets/Scripts/tap.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tap : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameManager.gameStart)
+						GetComponent<Renderer>().enabled = false;
+	
+	}
+}
